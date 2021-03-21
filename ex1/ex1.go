05a62eb2521b8ec505c0a92e8b3b56691a412147
svarunid <arun119019@gmail.com>
2021-03-21 15:00:41 +0530

    Completed Ninja Level 1!

---
+++ ex1/ex1.go
@@ -0,0 +1,20 @@
+//Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+//42
+//“James Bond”
+//true
+//Now print the values stored in those variables using
+//a single print statement
+//multiple print statements
+package main
+
+import "fmt"
+
+func main() {
+	x, y, z := 42, "JamesBond", true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
